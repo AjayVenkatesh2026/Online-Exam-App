9f76094505e4f9dff7ba86275b6bc10c20bfef03
Ajay Venkatesh Gunturu <O162026@rguktong.ac.in>
2021-07-12 15:33:25 +0530

    New Class Creation for teachers backend is completed.

--- app/src/main/java/com/ajayvenkateshgunturu/onlineexam/Teachers/CreateNewClassroomActivity.java
+++ app/src/main/java/com/ajayvenkateshgunturu/onlineexam/Teachers/CreateNewClassroomActivity.java
@@ -3,6 +3,7 @@ package com.ajayvenkateshgunturu.onlineexam.Teachers;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,13 +19,15 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Random;
+
 import static com.ajayvenkateshgunturu.onlineexam.Constants.FIREBASE_URL;
 
-public class CreateNewClassroomActivity extends AppCompatActivity implements checkClassExistsListener{
+public class CreateNewClassroomActivity extends AppCompatActivity{
 
     private TextInputEditText classNameEditText, classDescriptionEditText;
     private Button createClassButton;
-    public boolean result;
     DatabaseReference reference = FirebaseDatabase.getInstance(FIREBASE_URL).getReference();
     FirebaseAuth auth = FirebaseAuth.getInstance();
 
@@ -39,53 +42,113 @@ public class CreateNewClassroomActivity extends AppCompatActivity implements che
 
     }
 
-    private void setListeners() {
-        createClassButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int classId = 123456;
-                checkClassExistsListener l = CreateNewClassroomActivity.this;
-                l.doesClassExists(classId);
-                if(result){
-                    Toast.makeText(CreateNewClassroomActivity.this, "Result is true", Toast.LENGTH_SHORT).show();
-                }else{
-                    Log.e("In OnClick result", "false");
-                }
-                //FirebaseDatabase.getInstance(FIREBASE_URL).getReference("Classes").child(String.valueOf(classId)).child("admin").setValue(FirebaseAuth.getInstance().getUid());
-            }
-        });
-    }
-
     private void initViews() {
         classNameEditText = findViewById(R.id.text_input_edit_text_class_name);
         classDescriptionEditText = findViewById(R.id.text_input_edit_text_class_description);
         createClassButton = findViewById(R.id.button_create_class);
     }
 
-    public void doesClassExists(int classId){
-//        AtomicBoolean result = new AtomicBoolean(false);
-        result = false;
+
+    private void setListeners() {
+        createClassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int classId = generateRandomId(6);
+                String className = classNameEditText.getText().toString().trim();
+                String classDescription = classDescriptionEditText.getText().toString().trim();
+                if(className.isEmpty() || classDescription.isEmpty()){
+                    Toast.makeText(CreateNewClassroomActivity.this, "empty fields", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                doesClassExist(new checkClassExistsListener() {
+                    @Override
+                    public void setResult(boolean result) {
+                        if(result){
+                            Toast.makeText(CreateNewClassroomActivity.this, "Result is true", Toast.LENGTH_SHORT).show();
+                            Log.e("In OnClick result", "true");
+                        }else{
+                            Log.e("In OnClick result", "false");
+                            createNewClass(classId, className, classDescription);
+                        }
+                    }
+                }, classId);
+            }
+        });
+    }
+
+    private void createNewClass(int classId, String className, String classDescription) {
+        HashMap<String, Object > map = new HashMap<>();
+        map.put("classId", String.valueOf(classId));
+        map.put("className", className);
+        map.put("classDescription", classDescription);
+        map.put("admin", auth.getUid());
+        reference.child("Classes").child(String.valueOf(classId)).updateChildren(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Log.e("New Class", "Created");
+                    Toast.makeText(CreateNewClassroomActivity.this, "New Class Created", Toast.LENGTH_SHORT).show();
+                    uploadClassData(classId);
+                }else{
+                    Log.e("New Class", "Not Created");
+                    Toast.makeText(CreateNewClassroomActivity.this, "New Class Not Created", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private void uploadClassData(int classId){
+        reference.child("Teachers").child(auth.getUid()).child("Classes").child(String.valueOf(classId)).setValue("true").addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Log.e("Upload of Class Data", "Successful");
+                    startActivity(new Intent(getBaseContext(), TeachersMainActivity.class));
+                    finish();
+                }else{
+                    Log.e("Uploading of data", "Failure\nTrying Again..........");
+                    uploadClassData(classId);
+                }
+            }
+        });
+    }
+
+
+    private void doesClassExist(checkClassExistsListener listener, int classId){
         reference.child("Classes").child(String.valueOf(classId)).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
               @Override
               public void onComplete(@NonNull Task<DataSnapshot> task) {
                   if (task.isSuccessful()) {
                       DataSnapshot snapshot =  task.getResult();
-                      String res = "None";
-                      if (snapshot != null) {
-                          res = snapshot.getValue().toString();
-                          result = true;
+                      if (snapshot.getValue() != null) {
+                          listener.setResult(true);
                       } else {
+                          listener.setResult(false);
                           Toast.makeText(CreateNewClassroomActivity.this.getBaseContext(), "Class doesn't Exist", Toast.LENGTH_LONG).show();
                       }
-                      Log.e("Class Object", res);
-                      Log.e("In OnComplete result", String.valueOf(result));
                   }
               }
           }
         );
     }
+
+    public int generateRandomId(int num){
+        int n = 0;
+        Random rand = new Random();
+        for(int i = 0; i < num; i++) {
+            int t = rand.nextInt(10);
+            if(i == 0){
+                while(t == 0){
+                    t = rand.nextInt(10);
+                }
+            }
+            n = n*10 + t;
+        }
+        return n;
+    }
 }
 
 interface checkClassExistsListener{
-    void doesClassExists(int classId);
+    void setResult(boolean result);
 }
